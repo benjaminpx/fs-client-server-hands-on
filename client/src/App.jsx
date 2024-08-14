edd6649f95dfd74c5fe904eab466a7e7d640c032
asaf.cohen <asaf.cohen@perimeterx.com>
2024-08-14 18:35:18 +0300

    fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 const App = () => {
-    return <div>hel!lo</div>;
+    useEffect(() => {
+        fetch('http://localhost:5000/api')
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }, []);
+
+    return <div>hello world</div>;
 };
 
 export default App;
